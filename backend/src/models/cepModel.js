0a725f851a38081c9fe7732e9d74cbfec495d722
fehbarea <fehbarea@icloud.com>
2025-11-16 15:45:21 -0300

    feat: modelo cep

---
+++ backend/src/models/cepModel.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const cepSchema = new mongoose.Schema({
+	cep: {
+		type: String,
+		required: true,
+		unique: true,
+		match: /^\d{5}-?\d{3}$/,
+	},
+	logradouro: { type: String, default: '' },
+	bairro: { type: String, default: '' },
+	cidade: { type: String, default: '' },
+	uf: { type: String, default: '' },
+}, { timestamps: true });
+
+const Cep = mongoose.model('Cep', cepSchema, 'cep');
+
+export default Cep;
